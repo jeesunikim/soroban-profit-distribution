4631fc92c2051cfb2e21255a52a650e245de3969
Jeesun Kim <jeesunikim@users.noreply.github.com>
2023-01-18 23:54:12 -0500

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,43 +3,36 @@
 #![no_std]
 use soroban_auth::{Identifier, Signature};
 // importing the types and macros from soroban_sdk
-use soroban_sdk::{contractimpl, contracttype, symbol, vec, map, Env, Symbol, Vec, Map, BytesN, Address};
+use soroban_sdk::{contractimpl, contracttype, Env, Vec, BytesN};
 
 mod token {
     soroban_sdk::contractimport!(file = "./token/soroban_token_spec.wasm");
 }
 
-// @rust tip: importing test.rs
-mod test;
 
 pub struct ProfitDistributionContract;
 
 /*
 // Requirements:
 // 1. MeetupDate: The date of the meetup
-// 2. Balance: The total amount of deposits
-// 2. Depositers: People who deposited
-// 3. Attendees: Attendees who showed up and eligible to claim
-// 4. Started: The date the admin started collecting
-// 5. Admin: The person who can trigger the disbursement of the deposit
-// 6. Token: 
-// 7. User:
-// 8. Recipient: Meetup Pub Key
-// 9. Amount: The cost of deposit
+// 2. Attendees: Attendees who showed up and eligible to claim
+// 3. Started: The date the admin started collecting
+// 4. Admin: The person who can trigger the disbursement of the deposit
+// 5. Token: 
+// 6. User:
+// 7. DepositFee: The cost of deposit
 */
 #[derive(Clone)]
 #[contracttype]
 pub enum DataKey {
     MeetupDate,
     Balance,
-    Depositers,
     Attendees,
     Started,
     Admin,
     Token,
-    // User(Identifier),
-    Recipient,
-    Amount,
+    User(Identifier),
+    DepositFee,
 }
 
 #[derive(Clone)]
@@ -71,6 +64,7 @@ pub struct DepositBalance {
 // - Success = 1
 // - Expired = 2
 */
+
 #[derive(Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum State {
@@ -81,30 +75,28 @@ pub enum State {
 
 /*
 // Contract Usage Pattern (pseudocode):
-// 1. Call initialize(recipient, meetup_date_unix_epoch, target_amount, token) 
+// 1. Call initialize(recipient, meetup_date_unix_epoch, amount, token) 
 // 2. Depositor makes a deposit to this contract's address to REGISTER and the contract stores depositor's public key
-// 3. Once the meetup date is reached, the contract collects the attendees' public key, divides its total amount of balance by the # of attendees and send that amount to the attendees who match its depositors' public key
+// 3. Once the meetup date is reached, the contract (by admin) collects the attendees' public key, divides its total amount of balance by the # of attendees and send that amount to the attendees who match its depositors' public key
 */
 
 // @rust tip: #[contractimpl] where contract lives
 #[contractimpl]
 impl ProfitDistributionContract {
-    // @rust tip: any function that'll be called externally has 'pub'
+    // @rust tip: any function that'll be called externally use 'pub'
     pub fn initialize(
         env: Env,
-        recipient: Identifier,
-        balance: i128,
+        admin: Identifier,
         meetup_date: u64,
-        amount: i128,
+        deposit_fee: i128,
         token: BytesN<32>
     ){
         assert!(is_initialized(&env), "Contract already initialized");
 
-        env.storage().set(DataKey::Recipient, recipient);
+        env.storage().set(DataKey::Admin, admin);
         env.storage().set(DataKey::Started, get_ledger_timestamp(&env));
         env.storage().set(DataKey::MeetupDate, meetup_date);
-        env.storage().set(DataKey::Balance, balance);
-        env.storage().set(DataKey::Amount, amount);
+        env.storage().set(DataKey::DepositFee, deposit_fee);
         env.storage().set(DataKey::Token, token);
     }
 
@@ -119,8 +111,14 @@ impl ProfitDistributionContract {
             panic!("negative amount is not allowed")
         }
 
-        // Transfer token to this contract address.
-        transfer_from_account_to_contract(&env, &token, &env.invoker().into(), &amount);
+        /*  
+        // Transfer token to this contract address
+        // @soroban tip: The env.invoker() always returns the invoker of the currently executing contract. Returning either: 
+        // - Account with an AccountId if the contract was invoked directly by an account
+        // - Contract with a BytesN<32> contract ID if the contract was invoked by another contract
+        // https://soroban.stellar.org/docs/examples/auth#invoker
+        */
+        deposit_to_contract(&env, &env.invoker().into(), &amount);
         // Store all the necessary info to allow one of the claimants to claim it.
         env.storage().set(
             DataKey::Balance,
@@ -133,39 +131,83 @@ impl ProfitDistributionContract {
         );
     }
 
-    pub fn distribute(
+    pub fn distribute(env:Env){
+        let balance:DepositBalance = env.storage().get_unchecked(DataKey::Balance).unwrap();
 
-    ){}
+        let attendee_id = env.invoker().into();
+        let depositers= &balance.depositers;
+
+        if !depositers.contains(&attendee_id) {
+            panic!("this attendee didn't make a deposit to register for the meetup. They're not eligible to receive any deposit back");
+        }
+
+        // Transfer the stored amount of token to claimant after passing
+        // all the checks.
+        distribute_from_contract_to_account(
+            &env,
+            &attendee_id,
+            &balance.amount,
+        );
+        // Remove the balance entry to prevent any further claims.
+        env.storage().remove(DataKey::Balance);
+    }
 }
 
 fn is_initialized(env: &Env) -> bool {
-    env.storage().has(DataKey::Recipient)
+    env.storage().has(DataKey::Admin)
 }
 
-fn get_ledger_timestamp(e: &Env) -> u64 {
-    e.ledger().timestamp()
+fn get_ledger_timestamp(env: &Env) -> u64 {
+    env.ledger().timestamp()
 }
 
-fn get_contract_id(e: &Env) -> Identifier {
-    Identifier::Contract(e.get_current_contract())
+fn get_contract_id(env: &Env) -> Identifier {
+    Identifier::Contract(env.get_current_contract())
 }
 
-fn transfer_from_account_to_contract(
-    e: &Env,
-    token_id: &BytesN<32>,
-    from: &Identifier,
+fn get_token(env: &Env) -> BytesN<32> {
+    env.storage()
+        .get(DataKey::Token)
+        .expect("not initialized")
+        .unwrap()
+}
+
+fn get_balance(env: &Env, contract_id: &BytesN<32>) -> i128 {
+    let client = token::Client::new(env, contract_id);
+    client.balance(&get_contract_id(env))
+}
+
+fn deposit_to_contract(
+    env: &Env,
+    user: &Identifier,
     amount: &i128,
 ) {
-    let client = token::Client::new(e, token_id);
-    client.xfer_from(&Signature::Invoker, &0, from, &get_contract_id(e), amount);
+    let client = token::Client::new(env, &get_token(env));
+    let nonce: i128 = 0;
+    
+    /* 
+    // @soroban tips: client.xfer_from()
+    // xfer
+    // - an unprivileged mutator, which changes the state of the contract but do not require special privileges
+    // - a "sender" can use xfer to send money to a "admin" or contract id. For xfer, the sender must provide authorization
+    // invoker auth (&Signature::Invoker) is enough to use the built-in token with classic accounts
+    // more info on: https://soroban.stellar.org/docs/built-in-contracts/stellar-asset-contract#sac-operations &
+    // https://soroban.stellar.org/docs/common-interfaces/token
+    */ 
+    client.xfer_from(&Signature::Invoker,&nonce, user, &get_contract_id(env), amount);
 }
 
-fn transfer_from_contract_to_account(
-    e: &Env,
-    token_id: &BytesN<32>,
-    to: &Identifier,
+fn distribute_from_contract_to_account(
+    env: &Env,
+    user: &Identifier,
     amount: &i128,
 ) {
-    let client = token::Client::new(e, token_id);
-    client.xfer(&Signature::Invoker, &0, to, amount);
+
+    let client = token::Client::new(env, &get_token(env));
+    let nonce: i128 = 0;
+
+    client.xfer(&Signature::Invoker, &nonce, user, amount);
 }
+
+// @rust tip: importing test.rs
+mod test;
